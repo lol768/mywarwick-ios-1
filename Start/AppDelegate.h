2ccc940e8631fa49c0ca76f6064d1730c71794ef
Alec Cursley <alec@cursley.net>
2015-12-18 15:16:20 +0000

    Initial commit

---
+++ Start/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Start
+//
+//  Created by Alec Cursley on 15/12/2015.
+//  Copyright © 2015 University of Warwick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
