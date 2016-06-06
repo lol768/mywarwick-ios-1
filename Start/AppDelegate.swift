12b685974480399858d5400dee00f46ec35e1fd4
Alec Cursley <alec@cursley.net>
2016-06-06 11:36:39 +0100

    Allow configuring a custom start root URL

--- Start/AppDelegate.swift
+++ Start/AppDelegate.swift
@@ -22,13 +22,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             "deviceToken": token
         ])
     }
+    
+    func configuredDeploymentURL() -> NSURL? {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        if let deployment = defaults.stringForKey("StartDeployment") {
+            
+            if deployment == "custom" {
+                if let customDeployment = defaults.stringForKey("StartCustomDeployment") {
+                    if let url = NSURL(string: customDeployment) {
+                        print("Custom deployment URL: \(url)")
+                        return url
+                    } else {
+                        print("Invalid custom deployment URL: \(customDeployment)")
+                    }
+                }
+            }
+            
+            print("Configured standard deployment \(deployment)")
+            return NSURL(string: "https://\(deployment).warwick.ac.uk")
+        }
+        
+        return nil
+    }
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
-        if let deployment = NSUserDefaults.standardUserDefaults().stringForKey("StartDeployment") {
-            print("Switching to configured Start deployment \(deployment)")
-            Config.startURL = NSURL(string: "https://\(deployment).warwick.ac.uk")!
+        if let deployment = self.configuredDeploymentURL() {
+            Config.startURL = deployment
         }
         
         if launchOptions?[UIApplicationLaunchOptionsRemoteNotificationKey] != nil {
