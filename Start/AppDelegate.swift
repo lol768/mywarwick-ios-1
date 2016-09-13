b8589e2796c3837b5bad0eb26c28c322661378b8
Alec Cursley <alec@cursley.net>
2016-09-13 11:59:45 +0100

    Update to Swift 3

--- Start/AppDelegate.swift
+++ Start/AppDelegate.swift
@@ -13,54 +13,54 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
-    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         let tok = deviceToken.description
-        let token = tok.substringWithRange(tok.startIndex.successor()..<tok.endIndex.predecessor()).stringByReplacingOccurrencesOfString(" ", withString: "")
+        let token = tok.substring(with: tok.characters.index(after: tok.startIndex)..<tok.characters.index(before: tok.endIndex)).replacingOccurrences(of: " ", with: "")
         
-        NSNotificationCenter.defaultCenter().postNotificationName("DidRegisterForRemoteNotifications", object: self, userInfo: [
+        NotificationCenter.default.post(name: Notification.Name(rawValue: "DidRegisterForRemoteNotifications"), object: self, userInfo: [
             "deviceToken": token
             ])
     }
     
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
         if let url = Config.configuredDeploymentURL() {
             Config.startURL = url
         }
         
-        if launchOptions?[UIApplicationLaunchOptionsRemoteNotificationKey] != nil {
+        if launchOptions?[UIApplicationLaunchOptionsKey.remoteNotification] != nil {
             Global.didLaunchFromRemoteNotification = true
         }
         
         return true
     }
     
-    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
-        if (application.applicationState == .Inactive || application.applicationState == .Background) {
-            NSNotificationCenter.defaultCenter().postNotificationName("DidReceiveRemoteNotification", object: self, userInfo: nil)
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+        if (application.applicationState == .inactive || application.applicationState == .background) {
+            NotificationCenter.default.post(name: Notification.Name(rawValue: "DidReceiveRemoteNotification"), object: self, userInfo: nil)
         }
     }
     
-    func applicationWillResignActive(application: UIApplication) {
+    func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
     
-    func applicationDidEnterBackground(application: UIApplication) {
+    func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
     
-    func applicationWillEnterForeground(application: UIApplication) {
+    func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
     
-    func applicationDidBecomeActive(application: UIApplication) {
+    func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
     
-    func applicationWillTerminate(application: UIApplication) {
+    func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
--- Start/Config.swift
+++ Start/Config.swift
@@ -10,18 +10,18 @@ import Foundation
 
 class Config {
     
-    static var startURL = NSURL(string: "https://start.warwick.ac.uk")!
+    static var startURL = URL(string: "https://start-dev.warwick.ac.uk")!
     
     static let applicationNameForUserAgent = "WarwickStart/1.0"
     
-    static func configuredDeploymentURL() -> NSURL? {
-        let defaults = NSUserDefaults.standardUserDefaults()
+    static func configuredDeploymentURL() -> URL? {
+        let defaults = UserDefaults.standard
         
-        if let deployment = defaults.stringForKey("StartDeployment") {
+        if let deployment = defaults.string(forKey: "StartDeployment") {
             
             if deployment == "custom" {
-                if let customDeployment = defaults.stringForKey("StartCustomDeployment") {
-                    if let url = NSURL(string: customDeployment) {
+                if let customDeployment = defaults.string(forKey: "StartCustomDeployment") {
+                    if let url = URL(string: customDeployment) {
                         print("Custom deployment URL: \(url)")
                         return url
                     } else {
@@ -31,7 +31,7 @@ class Config {
             }
             
             print("Configured standard deployment \(deployment)")
-            return NSURL(string: "https://\(deployment).warwick.ac.uk")
+            return URL(string: "https://\(deployment).warwick.ac.uk")
         }
         
         return nil
