7615db396d4ebcc5be1cfb3bede489de7f9c3ea9
kai@shylock <k.lan@warwick.ac.uk>
2017-03-02 11:47:36 +0000

    NEWSTART-546 new view controller for account setting

---
+++ MyWarwick/AccountSettingViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AccountSettingViewController.swift
+//  MyWarwick
+//
+//  Created by Kai Lan on 02/03/2017.
+//  Copyright © 2017 University of Warwick. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SafariServices
+import WebKit
+
+class AccountSettingViewController: WebViewController {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if let url = navigationAction.request.url {
+            
+
+            // allow the redirect url
+            if (url.host == "warwick.ac.uk" || url.host == "www2.warwick.ac.uk") && url.path == "/myaccount" {
+                decisionHandler(.allow)
+                return
+            }
+            
+            // all everything on websignon
+            if url.host == Config.webSignOnURL.host {
+                decisionHandler(.allow)
+                return
+            }
+        }
+        decisionHandler(.allow)
+        delegate?.dismissWebView(sender: self)
+    }
+    
+    override func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        return super.presentFotTitle(webView, didFinish: navigation, pagetitle: "IT Services Account - Account settings")
+    }
+}
