0535c1f15d3c56fb984645929dd62ab11ef12b38
Alec Cursley <a.cursley@warwick.ac.uk>
2017-03-06 12:26:58 +0000

    NEWSTART-554 Handle app URL config consistently
    Happened when changing the app deployment from production to any other
    deployment without restarting in between.  Not a big deal as this
    functionality isn't in release builds, but fixed by ensuring we always
    access the current deployment URL in the same way (that updates
    whenever the preference is changed).
    
    Also a few minor code style tweaks, and removed some unnecessary uses of
    Optional.

--- MyWarwick/Helper.swift
+++ MyWarwick/Helper.swift
@@ -1,29 +1,21 @@
-//
-//  Helper.swift
-//  MyWarwick
-//
-//  Created by Kai Lan on 02/03/2017.
-//  Copyright © 2017 University of Warwick. All rights reserved.
-//
-
 import Foundation
 
 class Helper {
     
-    static func regexmatch(for regex: String, in text: String) -> [String] {
+    static func regexMatches(for regex: String, in text: String) -> [String] {
         do {
             let regex = try NSRegularExpression(pattern: regex)
             let nsString = text as NSString
             let results = regex.matches(in: text, range: NSRange(location: 0, length: nsString.length))
-            return results.map { nsString.substring(with: $0.range)}
+            return results.map { nsString.substring(with: $0.range) }
         } catch let error {
             print("invalid regex: \(error.localizedDescription)")
             return []
         }
     }
     
-    static func regexhave(for regex: String, in text: String) -> Bool {
-        return Helper.regexmatch(for: regex, in: text).count > 0
+    static func regexMatch(for regex: String, in text: String) -> Bool {
+        return !Helper.regexMatches(for: regex, in: text).isEmpty
     }
 
 }
