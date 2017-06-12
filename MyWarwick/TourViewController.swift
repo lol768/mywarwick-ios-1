63cfdd4f7193d19613ae7d859f86a9613e336431
Alec Cursley <a.cursley@warwick.ac.uk>
2017-06-12 16:57:43 +0100

    NEWSTART-730 Add x button

--- MyWarwick/TourViewController.swift
+++ MyWarwick/TourViewController.swift
@@ -5,6 +5,8 @@ class TourViewController: UIPageViewController, UIPageViewControllerDataSource {
 
     var tourPages: [UIViewController] = []
 
+    var finishButton: UIButton = UIButton(type: .system)
+
     override func viewDidLoad() {
         if storyboard != nil {
             tourPages = [1, 2, 3, 4, 5, 6, 7, 8].map { (n: Int) -> UIViewController in
@@ -13,10 +15,23 @@ class TourViewController: UIPageViewController, UIPageViewControllerDataSource {
         }
 
         dataSource = self
-
         view.backgroundColor = UIColor.white
 
+        finishButton.addTarget(self, action: #selector(finish), for: .touchUpInside)
+        finishButton.setImage(UIImage(named: "Cancel"), for: .normal)
+        finishButton.translatesAutoresizingMaskIntoConstraints = false
+        finishButton.tintColor = UIColor.black.withAlphaComponent(0.6)
 
+        view.addSubview(finishButton)
+
+        view.addConstraints([
+                NSLayoutConstraint(item: finishButton, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 60),
+                NSLayoutConstraint(item: finishButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 60),
+                NSLayoutConstraint(item: finishButton, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1, constant: 20),
+                NSLayoutConstraint(item: finishButton, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1, constant: 0)
+        ])
+
+        view.bringSubview(toFront: finishButton)
 
         if let firstPage = tourPages.first {
             setViewControllers([firstPage], direction: .forward, animated: true, completion: nil)
