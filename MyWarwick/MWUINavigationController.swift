abbe0f9433797161edb298bcf65d782c68d0e7a9
kai@shylock <k.lan@warwick.ac.uk>
2017-09-25 14:48:42 +0100

    NEWSTART-1085 subclass UINavController to over come a bug where it dismisses itself when it should not

---
+++ MyWarwick/MWUINavigationController.swift
@@ -0,0 +1,23 @@
+//
+//  MWUINavigationController.swift
+//  MyWarwick
+//
+//  Created by Kai Lan on 25/09/2017.
+//  Copyright © 2017 University of Warwick. All rights reserved.
+//
+
+import Foundation
+import WebKit
+
+class MWUINavigationController: UINavigationController {
+    
+    // apparently there is a bug in iOS 11 where UINavigationController dismisses itself when it should not
+    // this bug is fixed in iOS 11.
+    // this override func guards against this issue buy checking if itself is preseneted, if true it should be dismissed
+    override func dismiss(animated flag: Bool, completion: (() -> Void)? = nil) {
+        if (self.presentedViewController != nil) {
+            super.dismiss(animated: flag, completion: completion)
+        }
+    }
+    
+}
