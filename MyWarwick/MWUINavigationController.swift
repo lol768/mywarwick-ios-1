16a622c8628092870a84cd486c506bc8a0876d3e
kai@shylock <k.lan@warwick.ac.uk>
2017-09-25 15:23:35 +0100

    NEWSTART-1085 fix another typo

--- MyWarwick/MWUINavigationController.swift
+++ MyWarwick/MWUINavigationController.swift
@@ -13,7 +13,7 @@ class MWUINavigationController: UINavigationController {
     
     // apparently there is a bug in iOS 10 where UINavigationController dismisses itself when it should not
     // this bug is fixed in iOS 11.
-    // this override func guards against this issue by checking if itself is presented, so that only when ((!presented && flag == true ) == true) it should not be dismissed.
+    // this override func guards against this issue by checking if itself is presented, so that only when ((!presented && flag == true ) == true) it should be dismissed.
     override func dismiss(animated flag: Bool, completion: (() -> Void)? = nil) {
         if (self.presentedViewController != nil) {
             super.dismiss(animated: flag, completion: completion)
