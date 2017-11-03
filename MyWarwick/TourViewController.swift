6612b5f28a04366dc822d54fbea5268475d9d95a
Kai Lan <k.lan@warwick.ac.uk>
2017-11-03 16:51:43 +0000

    NEWSTART-1053 adjust tour view's page indicator location

--- MyWarwick/TourViewController.swift
+++ MyWarwick/TourViewController.swift
@@ -45,7 +45,7 @@ class TourViewController: UIPageViewController, UIPageViewControllerDelegate, UI
         view.addConstraints([
                 NSLayoutConstraint(item: pageControl, attribute: .left, relatedBy: .equal, toItem: view, attribute: .left, multiplier: 1, constant: 0),
                 NSLayoutConstraint(item: pageControl, attribute: .width, relatedBy: .equal, toItem: view, attribute: .width, multiplier: 1, constant: 0),
-                NSLayoutConstraint(item: pageControl, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1, constant: 0),
+                NSLayoutConstraint(item: pageControl, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1, constant: -10),
                 NSLayoutConstraint(item: pageControl, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 24)
         ])
 
