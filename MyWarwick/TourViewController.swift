ea88c7ee9a282b5f517cf29804e0cc7b5246d85c
Kai Lan <k.lan@warwick.ac.uk>
2017-11-08 15:02:41 +0000

    NEWSTART-1053 adjust to use safearea for positioning page control

--- MyWarwick/TourViewController.swift
+++ MyWarwick/TourViewController.swift
@@ -42,10 +42,15 @@ class TourViewController: UIPageViewController, UIPageViewControllerDelegate, UI
         pageControl.addTarget(self, action: #selector(changePage), for: .valueChanged)
         view.addSubview(pageControl)
 
+        var pageControlBottomConstraint =  NSLayoutConstraint(item: pageControl, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1, constant: 0)
+        if #available(iOS 11, *) {
+            pageControlBottomConstraint = NSLayoutConstraint(item: pageControl, attribute: .bottom, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .bottom, multiplier: 1, constant: 0)
+        }
+        
         view.addConstraints([
                 NSLayoutConstraint(item: pageControl, attribute: .left, relatedBy: .equal, toItem: view, attribute: .left, multiplier: 1, constant: 0),
                 NSLayoutConstraint(item: pageControl, attribute: .width, relatedBy: .equal, toItem: view, attribute: .width, multiplier: 1, constant: 0),
-                NSLayoutConstraint(item: pageControl, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1, constant: -10),
+                pageControlBottomConstraint,
                 NSLayoutConstraint(item: pageControl, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 24)
         ])
 
