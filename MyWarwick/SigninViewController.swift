48756724828f16e8b891d741e59681ee8597cb45
Kai Lan <k.lan@warwick.ac.uk>
2018-02-14 11:43:13 +0000

    NEWSTART-1330 open a safariwebviewcontroller in singinviewcontroller for other links

--- MyWarwick/SigninViewController.swift
+++ MyWarwick/SigninViewController.swift
@@ -1,10 +1,27 @@
 import Foundation
 import UIKit
 import SafariServices
+import SafariServices
 import WebKit
 
 class SigninViewController: WebViewController {
 
+    func presentSafariWebView(_ url: URL) {
+        let svc = SFSafariViewController(url: url)
+        
+        if UIDevice.current.systemVersion.hasPrefix("9.2") {
+            // Workaround for a bug in iOS 9.2 - see https://forums.developer.apple.com/thread/29048#discussion-105377
+            self.modalPresentationStyle = .overFullScreen
+            
+            let nvc = UINavigationController(rootViewController: svc)
+            nvc.isNavigationBarHidden = true
+            
+            present(nvc, animated: true)
+        } else {
+            present(svc, animated: true)
+        }
+    }
+    
     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
         if let url = navigationAction.request.url {
             // sign in
@@ -16,6 +33,13 @@ class SigninViewController: WebViewController {
             // go to my warwick
             if url.host == Config.appURL.host && url.path == "/sso/acs" {
                 decisionHandler(.allow)
+                return
+            }
+            
+            // handle other links e.g. t&c etc.
+            if url.host != Config.appURL.host {
+                decisionHandler(.cancel)
+                presentSafariWebView(url)
                 return
             }
         }
