b78d7631316fbe0def7bb57246a39bb140197c8c
Kai Lan <k.lan@warwick.ac.uk>
2018-02-27 16:06:48 +0000

    NEWSTART-1366 fix incorrect implementation here

--- MyWarwick/UserNotificationController.swift
+++ MyWarwick/UserNotificationController.swift
@@ -13,25 +13,7 @@ import UserNotifications
 @available(iOS 10.0, *)
 class UserNotificationController: NSObject, UNUserNotificationCenterDelegate {
 
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-        let remoteNotification = response.notification
-        let priority = response.notification.request.content.userInfo["priority"] as? String ?? "normal"
-        print("here!! priority")
-        print(priority)
-        if priority == "high"  {
-            // now we create a local notification with high priority
-            let notification = UILocalNotification()
-            let content = remoteNotification.request.content
-            notification.alertTitle = content.title
-            notification.alertBody = content.body
-            notification.userInfo = ["priority": "high"]
-            UIApplication.shared.scheduleLocalNotification(notification)
-        }
-        completionHandler()
-    }
-    
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        print("local ones")
         let priority = notification.request.content.userInfo["priority"] as? String ?? "normal"
         
         if priority == "high"  {
