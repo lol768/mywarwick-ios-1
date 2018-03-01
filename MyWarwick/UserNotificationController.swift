79037ff8bea5b5097cbdf779063652efc347e723
Kai Lan <lk9100@mac.com>
2018-03-01 16:29:00 +0000

    NEWSTART-1366  move all logic here

--- MyWarwick/UserNotificationController.swift
+++ MyWarwick/UserNotificationController.swift
@@ -13,11 +13,31 @@ import UserNotifications
 @available(iOS 10.0, *)
 class UserNotificationController: NSObject, UNUserNotificationCenterDelegate {
 
+    let viewController: ViewController?
+    
+    init(viewController: ViewController) {
+        self.viewController = viewController
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let state = UIApplication.shared.applicationState
+        if (state == .inactive || state == .background)  {
+            if (response.notification.request.content.userInfo["transient"] as? Bool ?? false) {
+                let content = response.notification.request.content
+                NotificationCenter.default.post(name: Notification.Name(rawValue: "DidReceivetransientRemoteNotification"), object: self, userInfo: ["title": content.title, "body": content.body])
+            } else {
+                NotificationCenter.default.post(name: Notification.Name(rawValue: "DidReceiveRemoteNotification"), object: self, userInfo: nil)
+            }
+        }
+    }
+    
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         let priority = notification.request.content.userInfo["priority"] as? String ?? "normal"
         
         if priority == "high"  {
-            completionHandler([.alert, .badge, .sound])
+            if (notification.request.content.userInfo["transient"] as? Bool ?? false) {
+                completionHandler([.badge, .sound, .alert])
+            } 
         } else {
             completionHandler([.badge])
         }
